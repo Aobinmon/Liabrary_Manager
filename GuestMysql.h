bef6dd6b8266d2c6d0698777e9dcc3f4dd1b1bf0
hannawong <39270952+hannawong@users.noreply.github.com>
2018-06-10 16:44:36 +0800

    Update GuestMysql.h

--- GuestMysql.h
+++ GuestMysql.h
@@ -5,10 +5,7 @@
 #include "Guest.h"
 class GuestMysql:public UserMysql{
 public:
-    bool existUsername(QString username);
-    QString getPassword(QString username);
     void modifyPassword(QString password);
-    std::vector<Guest*> find_guest(QString type, QString feature);
 };
 
 #endif // GUESTMYSQL_H

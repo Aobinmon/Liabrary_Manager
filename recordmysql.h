0d05c9c1773bdd2735930ec1a7ac1ae641032507
wu-jy17 <39238330+wu-jy17@users.noreply.github.com>
2018-06-10 17:11:49 +0800

    Update recordmysql.h

--- recordmysql.h
+++ recordmysql.h
@@ -1,26 +1,12 @@
-//
-// Created by wujiayi on 2018/5/8.
-//
-
-#ifndef LIBRAR_RECORD_H
-#define LIBRAR_RECORD_H
-
-#include <string>
-#include "Guest.h"
-#include "Book.h"
-#include "vector"
-#include <Mysql.h>
-#include "Administrator.h"
-
-class Record {
-    int ID;
-    std::string time;
-    GuestMysql gm;
-    RecordMysql rm;
-    BookMysql bm;
-public:
-    Record(Guest* _guest, Book* _book);
-};
-
-
-#endif //LIBRAR_RECORD_H
++#ifndef RECORDMYSQL_H
+ +#define RECORDMYSQL_H
+ +#include "Mysql.h"
+ +#include "Record.h"
+ +#include <vector>
+ +#include <QString>
+ +class RecordMysql : public Mysql{
+ +public:
+ +    std::vector<Record*> findRecord(QString Usertype, QString infotype, QString info);
+ +};
+ +
+ +#endif // RECORDMYSQL_H

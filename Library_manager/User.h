8b13ac478573e5f5f0e84335f7feb539cc667072
wu-jy17 <39238330+wu-jy17@users.noreply.github.com>
2018-06-10 17:22:00 +0800

    Update User.h

--- Library_manager/User.h
+++ Library_manager/User.h
@@ -1,24 +1,19 @@
-#ifndef USER_H
-#define USER_H
+//
+// Created by wujiayi on 2018/5/8.
+//
+
+#ifndef LIBRAR_USER_H
+#define LIBRAR_USER_H
 #include <string>
 #include <map>
-class User{
-	int ID;
-	std::string Password;
-	std::string Name;
-public:
-	static std::map<int, std::string> U_IP;
-	virtual ~User();
-	int getID();
-	std::string getName();
-	void modify_password(std::string pw);
-	virtual void set_password(std::string pw) = 0;
-	
 
-	void find_book_by_ID(int id);
-	void find_book_by_BookName(std::string bookName);
-	void find_book_by_WriterName(std::string writerName);
-	void find_book_by_feature(std::string feature);
+class User {
+    int ID;
+    std::string Password;
+    std::string Name;
+public:
+    virtual ~User();
 };
 
-#endif
+
+#endif //LIBRAR_USER_H

033fd53632187ad736f27a7d6d69a20ebcecade7
wu-jy17 <39238330+wu-jy17@users.noreply.github.com>
2018-06-10 17:15:25 +0800

    Update Administrator.h

--- Library_manager/Administrator.h
+++ Library_manager/Administrator.h
@@ -1,29 +1,35 @@
-#ifndef ADMINISTRATOR_H
-#define ADMINISTRATOR_H
+//
+// Created by wujiayi on 2018/5/8.
+//
 
-#include <map>
-#include <string>
+#ifndef LIBRAR_GUEST_H
+#define LIBRAR_GUEST_H
+
 #include "User.h"
-#include "Guest.h"
-#include "Record.h"
 #include "Book.h"
-class Administrator : public User{
-	
-public:
-	void set_password(std::string pw);
-	void modify_Gpassword(std::string pw, Guest* guest);
-	void ban_Guest(Guest* guest);
-	void start_Guest(Guest* guest);
-	
-	void approve(Record* record, Guest* guest, Book* book);
-	void refuse(Record* record, int reason);                                // reason: 1--借书数目超过上限， 2---权限不够借阅该类书籍， 3---未知错误（管理员心情不好.......sorry, 身为管理员就是可以为所欲为
-	void find_Record_by_ID(int id);
-	void find_Record_by_BookName(std::string bookname);
-	void find_Record_by_GuestName(std::string guestname);
-	void find_Record_by_BookID(int bid);
-	void find_Record_by_GuestID(int gid);
+#include <string>
+#include <vector>
+#include "Administrator.h"
+#include <Mysql.h>
 
-	void addBook(Book* book);
-	void deleteBook(Book* book);
+class Guest : public User{
+	int bBook{0};
+    BookMysql bm;
+    GuestMysql gm;
+    RecordMysql rm;
+    To-doMysql tm;
+public:
+    Guest(std::string name, std::string password);//直接在创建时设置bBook = 0
+    friend class Administrator;
+    void Applyforbook(Book* book);//申请借书
+    void ApplyreturnBook(Book* book);//申请还书
+    void collection(Book* book);//收藏书
+    std::vector<Book*> findbook(std::string type, std::string value);
+    ~Guest();//暂时没写
 };
-#endif
+
+
+
+
+
+#endif //LIBRAR_GUEST_H

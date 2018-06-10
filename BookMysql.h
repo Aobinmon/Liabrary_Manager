b462bd32ed5c9a56468db722826bab38a31eb3ca
hannawong <39270952+hannawong@users.noreply.github.com>
2018-06-10 17:29:33 +0800

    Update BookMysql.h

--- BookMysql.h
+++ BookMysql.h
@@ -3,10 +3,9 @@
 #include "Mysql.h"
 #include "Book.h"
 class BookMysql : public Mysql{
-    void modifyInfo(QString column, QString info);
+    bool modifyInfo(QString column0, QString info0,QString column1,Qstring info1);
     void insertBook(Book* book);
-    void dropBook(Book* book);
-    void modifyBook(Book* book);
+    bool dropBook(Book* book);
 };
 
 #endif // BOOKMYSQL_H

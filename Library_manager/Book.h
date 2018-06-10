2ff9834bf65491fa3eb9df7aac419589134a93ff
wu-jy17 <39238330+wu-jy17@users.noreply.github.com>
2018-06-10 17:17:35 +0800

    Update Book.h

--- Library_manager/Book.h
+++ Library_manager/Book.h
@@ -1,36 +1,26 @@
-#ifndef BOOK_H
-#define BOOK_H
+//
+// Created by wujiayi on 2018/5/8.
+//
+
+#ifndef LIBRAR_BOOK_H
+#define LIBRAR_BOOK_H
+
 #include "Administrator.h"
 #include <string>
-#include <map>
-class Book{
-	std::string BookName;
-	int ID;                                                      //6位，前两位代表类型，中间两位代表特点，最后两位代表编号
-	std::string WriterName;
-	std::string Press;                         //出版社  
-	std::string PressTime;                     //出版时间
-	bool Onshelf;                              //是否在架
-	std::string Position;                      //馆藏位置
-	std::string Summary;                       //简介
-	std::string Type;                          //类型
+class Book {
+    BookMysql bm;
+    std::string BookName;
+    int ID;                                                      //6位，前两位代表类型，中间两位代表特点，最后两位代表编号
+    std::string WriterName;
+    std::string Press;                         //出版社
+    std::string PressTime;                     //出版时间
+    int Onshelf;                              //几本在架
+    std::string Position;                      //馆藏位置
+    std::string Summary;                       //简介
+    std::string Type;                          //类型
 public:
-	Book(std::string BookName, std::string WriterName, std::string Press, std::string PressTime);
-	static std::map<std::string, int> FEA;
-	int getID();
-	std::string getWriterName();
-	std::string getPress();
-	std::string getPressTime();
-	std::string getPosition();
-	std::string getSummary();
-	std::string getBookName();
-	bool operator== (Book* book);
-	void PutonShelf(std::string position);
-	void PutoffShelf();
-	void modifyID(int id);
-	bool getOnshelf();
-	virtual std::string getType() = 0;
-	virtual std::string getFeature() = 0;
-	virtual ~Book() {};
+    Book(std::string BookName, std::string WriterName, std::string Press, std::string PressTime);
 };
 
-#endif
+
+#endif //LIBRAR_BOOK_H

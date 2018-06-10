c0d2d778a4e82decfd6bd1935c4388ed4d769307
wu-jy17 <39238330+wu-jy17@users.noreply.github.com>
2018-06-10 17:03:24 +0800

    Update GuestMysql.h

--- GuestMysql.h
+++ GuestMysql.h
@@ -1,35 +1,10 @@
-//
-// Created by wujiayi on 2018/5/8.
-//
-
-#ifndef LIBRAR_GUEST_H
-#define LIBRAR_GUEST_H
-
-#include "User.h"
-#include "Book.h"
-#include <string>
-#include <vector>
-#include "Administrator.h"
-#include <Mysql.h>
-
-class Guest : public User{
-	int bBook{0};
-    BookMysql bm;
-    GuestMysql gm;
-    RecordMysql rm;
-    To-doMysql tm;
-public:
-    Guest(std::string name, std::string password);//直接在创建时设置bBook = 0
-    friend class Administrator;
-    void Applyforbook(Book* book);//申请借书
-    void ApplyreturnBook(Book* book);//申请还书
-    void collection(Book* book);//收藏书
-    std::vector<Book*> findbook(std::string type, std::string value);
-    ~Guest();//暂时没写
-};
-
-
-
-
-
-#endif //LIBRAR_GUEST_H
+#include "Guest.h"
+  class GuestMysql:public UserMysql{
+  public:
+ -    bool existUsername(QString username);
+ -    QString getPassword(QString username);
+      void modifyPassword(QString password);
+ -    std::vector<Guest*> find_guest(QString type, QString feature);
+  };
+  
+  #endif // GUESTMYSQL_H

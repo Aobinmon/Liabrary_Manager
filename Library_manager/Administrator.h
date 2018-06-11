03a84502e00f6007ff440db790e0986d1cf72107
wu-jy17 <39238330+wu-jy17@users.noreply.github.com>
2018-06-11 18:23:18 +0800

    Update Administrator.h

--- Library_manager/Administrator.h
+++ Library_manager/Administrator.h
@@ -2,34 +2,36 @@
 // Created by wujiayi on 2018/5/8.
 //
 
-#ifndef LIBRAR_GUEST_H
-#define LIBRAR_GUEST_H
+#ifndef LIBRAR_ADIMINISTRATOR_H
+#define LIBRAR_ADIMINISTRATOR_H
 
-#include "User.h"
-#include "Book.h"
 #include <string>
+#include "User.h"
+#include "Guest.h"
+#include "Record.h"
+#include "Book.h"
 #include <vector>
-#include "Administrator.h"
-#include <Mysql.h>
-
-class Guest : public User{
-	int bBook{0};
-    BookMysql bm;
-    GuestMysql gm;
+#include "Mysql.h"
+class Administrator : public User{
+    int ID;
+    GusetMysql gm;
+    AdministratorMysql am;
     RecordMysql rm;
-    To-doMysql tm;
+    BookMysql bm;
+    To_doMysql tm;
+    std::string username;
+    std::string password;
 public:
-    Guest(std::string name, std::string password);//直接在创建时设置bBook = 0
-    friend class Administrator;
-    void Applyforbook(Book* book);//申请借书
-    void ApplyreturnBook(Book* book);//申请还书
-    void collection(Book* book);//收藏书
-    std::vector<Book*> findbook(std::string type, std::string value);
-    ~Guest();//暂时没写
+
+    bool approve(Record* record);//在库数减一
+    bool refuse(Record* record, int reason);                                // reason: 1--借书数目超过上限， 2---权限不够借阅该类书籍， 3---未知错误（管理员心情不好.......sorry, 身为管理员就是可以为所欲为
+    std::vector<Record*> find_record(std::string type, std::string value); //type包括时间，序号，书名
+    std::vector<Guest*> find_guest(std::string type, std::string value);  //type包括用户名，ID
+    void addBook(Book* book);//书库中加书
+    void deleteBook(Book* book);//书库中减书
+    bool returnBook(Record* record);//管理员同意还书申请:在库数加一
+    ~Administrator();//暂时没写
 };
 
 
-
-
-
-#endif //LIBRAR_GUEST_H
+#endif //LIBRAR_ADIMINISTRATOR_H

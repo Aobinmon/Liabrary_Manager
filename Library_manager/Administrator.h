549cf984ee59a6758cf689deee9c7be75e5baf36
wu-jy17 <39238330+wu-jy17@users.noreply.github.com>
2018-06-11 21:49:20 +0800

    Update Administrator.h

--- Library_manager/Administrator.h
+++ Library_manager/Administrator.h
@@ -23,13 +23,13 @@ class Administrator : public User{
     std::string password;
 public:
 
-    void approve(Record* record);//在库数减一
-    void refuse(Record* record);                                // reason: 1--借书数目超过上限， 2---权限不够借阅该类书籍， 3---未知错误（管理员心情不好.......sorry, 身为管理员就是可以为所欲为
+    bool approve(Record* record);//在库数减一
+    bool refuse(Record* record, int reason);                                // reason: 1--借书数目超过上限， 2---权限不够借阅该类书籍， 3---未知错误（管理员心情不好.......sorry, 身为管理员就是可以为所欲为
     std::vector<Record*> find_record(std::string type, std::string value); //type包括时间，序号，书名
     std::vector<Guest*> find_guest(std::string type, std::string value);  //type包括用户名，ID
     void addBook(Book* book);//书库中加书
     void deleteBook(Book* book);//书库中减书
-    void returnBook(Record* record);//管理员同意还书申请:在库数加一
+    bool returnBook(Record* record);//管理员同意还书申请:在库数加一
     ~Administrator();//暂时没写
 };
 

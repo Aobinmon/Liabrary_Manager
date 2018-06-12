94eb8c33b055630c846b5944e650ad6da5cf12a2
wu-jy17 <39238330+wu-jy17@users.noreply.github.com>
2018-06-12 23:14:07 +0800

    Update Record.h

--- Library_manager/Record.h
+++ Library_manager/Record.h
@@ -15,11 +15,16 @@
 class Record {
     int ID;
     std::string time;
+    int book_id;
+    int guest_id;
+    std::string type;
+    std::string result;
     GuestMysql gm;
     RecordMysql rm;
     BookMysql bm;
 public:
     Record(Guest* _guest, Book* _book);
+    Record(int _ID, int _book_id, int _guest_id, std::string _type, std::string _result);
 };
 
 

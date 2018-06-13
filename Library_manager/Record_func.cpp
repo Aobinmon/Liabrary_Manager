6ea88f5f08ce58f35eba508226952bb2fda93cc6
wu-jy17 <39238330+wu-jy17@users.noreply.github.com>
2018-06-13 17:45:49 +0800

    Update Record_func.cpp

--- Library_manager/Record_func.cpp
+++ Library_manager/Record_func.cpp
@@ -12,15 +12,35 @@
 
 using namespace std;
 
-Record::Record(Guest *_guest, Book *_book) {
-    //查找guestID、bookID
-    return rm.addrecord(QString::fromStdString(guestID),QString::fromStdString(bookID));
-}
-
-Record::Record(int _ID, int _book_id, int _guest_id, std::string _type, std::string _result) {
+Record::Record(int _ID, int _book_id, int _guest_id, std::string _type, std::string _result, std::string _time) {
     ID = _ID;
     book_id = _book_id;
     guest_id = _guest_id;
     type = _type;
     result = _result;
+    time = _time;
+}
+
+int Record::getID() {
+    return ID;
+}
+
+Guest* Record::getguest() {
+    return _guest;
+}
+
+Book* Record::getbook() {
+    return _book;
+}
+
+std::string Record::getresult() {
+    return result;
+}
+
+std::string Record::gettime() {
+    return time;
+}
+
+std::string Record::gettype() {
+    return type;
 }

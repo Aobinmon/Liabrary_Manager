3e9c279e5a031b95c6cb510b0d9a00a53c3df37f
wu-jy17 <39238330+wu-jy17@users.noreply.github.com>
2018-06-13 17:51:15 +0800

    Create User.cpp

---
+++ Library_manager/User.cpp
@@ -0,0 +1,16 @@
+//
+// Created by wujiayi on 2018/6/13.
+//
+#include "User.h"
+void User::modifyid(int id) {
+    ID = id;
+}
+
+void User::modifyname(std::string _name) {
+    Name = _name;
+}
+
+void User::modifypassword(std::string _password) {
+    Password = _password;
+}
+

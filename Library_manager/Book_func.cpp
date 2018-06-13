a2a96c792ad4d8d5041e6e33467783b8bad4c37d
wu-jy17 <39238330+wu-jy17@users.noreply.github.com>
2018-06-13 17:39:56 +0800

    Update Book_func.cpp

--- Library_manager/Book_func.cpp
+++ Library_manager/Book_func.cpp
@@ -10,9 +10,46 @@
 
 using namespace std;
 
-Book::Book(string bookName, string writerName, string press, string pressTime){
-    BookName = bookName;
-    WriterName = writerName;
-    Press = press;
-    PressTime = pressTime;
+Book::Book(int id, std::string _BookName, std::string _WriterName, std::string _Press, std::string _PressTime) {
+    ID = id;
+    BookName = _BookName;
+    WriterName = _WriterName;
+    Press = _Press;
+    PressTime = _PressTime;
+}
+
+int Book::getID() {
+    return ID;
+}
+
+std::string Book::getBookName() {
+    return BookName;
+}
+
+bool Book::getOnshelf() {
+    return Onshelf;
+}
+
+std::string Book::getPosition() {
+    return Position;
+}
+
+std::string Book::getPress() {
+    return Press;
+}
+
+std::string Book::getPressTime() {
+    return PressTime;
+}
+
+std::string Book::getSummary() {
+    return Summary;
+}
+
+std::string Book::getType() {
+    return Type;
+}
+
+std::string Book::getWriterName() {
+    return WriterName;
 }

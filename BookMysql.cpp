2fe9aa3fd4b1137e21b341c7c54dd06b0c573109
hannawong <39270952+hannawong@users.noreply.github.com>
2018-06-14 20:19:22 +0800

    Update BookMysql.cpp

--- BookMysql.cpp
+++ BookMysql.cpp
@@ -1,10 +1,11 @@
 #include"BookMysql.h"
 
 using namespace std;
-
 bool BookMysql::modifyInfo(QString column0, QString info0,QString column1,Qstring info1){
     //找到column0是info0的地方，修改column1为info1
     QSqlQuery query;
+    query.exec("select * from Books where column0 =info0");
+    QString id=query.value(0).toString();
     if(column0=="ID"&&column1=="ID")//因为要区分int型和string型的写法
         query.exec("update Books set "+column1+"="+info1+" where"+column0+"="+info0);
     if(column0=="ID"&&column1!="ID")
@@ -14,10 +15,10 @@ bool BookMysql::modifyInfo(QString column0, QString info0,QString column1,Qstrin
     else 
         query.exec("update Books set "+column1+"=\""+info1+"\" where"+column0+"=\""+info0+"\"");
     
-    if(query.next()) return true;
+    query.exec("select "+column1 +" from Books where ID="+id);
+    if(query.value(0)==info1) return true;
     else return false;
 }
-
 void BookMysql::insertBook(Book *book){
     Qstring id=book->getID();
     Qstring bookname=book->getBookName();
